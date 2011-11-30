8d1d34b470e03b1be628e813f2480079133417e8
Robert Williams <rob@r-williams.com>
2011-11-30 13:28:39 +0000

    Revert "ha"
    This reverts commit 1e85cde270cb65b8701239ed88d4ba83ce015a05.

--- spec/chimp_contact_spec.rb
+++ spec/chimp_contact_spec.rb
@@ -15,30 +15,13 @@ module ChimpContact
   end
 end
 
-describe ChimpContact::StripMailChimpAttributes, "#convert" do
+describe ChimpContact::StripMailChimpAttributes do
   
-  context "with one mc: attribute" do
-    it 'should remove mc: attribute from link' do
-      @smc = ChimpContact::StripMailChimpAttributes.new("<a href='#' mc:editable='link'></a>")
-      @smc.convert.should eql("<a href='#' ></a>")
-    end
+  before do
+    @smc = ChimpContact::StripMailChimpAttributes.new("<a href='#' mc:editable='link'></a>")
   end
 
-  context "with multiple mc: attributes" do
-    it 'should remove all mc: attributes from link' do
-      @smc = ChimpContact::StripMailChimpAttributes.new(
-        "<a href='#' mc:hideable='fred' mc:editable='link'></a>"
-      )
-      @smc.convert.should eql("<a href='#' ></a>")
-    end
-  end
-
-  context "with multiple tags with multiple mc: attributes" do
-    it 'should remove all mc: attributes from all links' do
-      @smc = ChimpContact::StripMailChimpAttributes.new(
-        ("<a href='#' mc:hideable='fred' mc:editable='link'></a>") * 5
-      )
-      @smc.convert.should eql("<a href='#' ></a>" * 5)
-    end
+  it 'should find attributes that start with mc: and remove them' do
+    @smc.convert.should eql("<a href='#' ></a>")
   end
 end
