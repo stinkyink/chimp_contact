8f70a8a5c1618647d92491e2075fb8a729ac6b7a
Robert Williams <rob@r-williams.com>
2011-11-30 14:14:01 +0000

    Added copyright addition

--- spec/chimp_contact_spec.rb
+++ spec/chimp_contact_spec.rb
@@ -3,6 +3,14 @@ require 'nokogiri'
 module ChimpContact
   
   class Convertor
+
+    COPYRIGHT = [
+      'Copyright (c) 1996-2011 Constant Contact. All rights reserved.  Except as permitted under a separate',
+      'written agreement with Constant Contact, neither the Constant Contact software, nor any content that appears on any Constant Contact site,',
+      'including but not limited to, web pages, newsletters, or templates may be reproduced, republished, repurposed, or distributed without the',
+      'prior written permission of Constant Contact.  For inquiries regarding reproduction or distribution of any Constant Contact material, please',
+      'contact legal@constantcontact.com.'
+    ]
     
     def initialize(document)
       @document = document
@@ -10,12 +18,20 @@ module ChimpContact
     
     def convert
       strip_mail_chimp_attributes
+      insert_copyright
       @document
     end
     
     protected
     def strip_mail_chimp_attributes
       @document.xpath('//*').each { |e| e.attributes.each { |k,v| v.remove if k =~ /^mc/ } }
+    end
+    
+    def insert_copyright
+      body_tag = @document.at_xpath('//body')
+      copyright_tag = Nokogiri::XML::Node.new("CopyRight", @document)
+      copyright_tag.content = COPYRIGHT.join("\n")
+      body_tag.add_next_sibling(copyright_tag)
     end
   end
 end
@@ -30,5 +46,9 @@ describe ChimpContact::Convertor do
 
   it 'should find attributes that start with mc: and remove them' do
     should include('<a href="#"></a>')
+  end
+  
+  it 'should add the copyright tag just after the body tag' do
+    should include("<CopyRight>")
   end
 end
